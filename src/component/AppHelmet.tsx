38264f5b81c2dd7b6e93a97b1aa2a6abb71eb13e
Swastik <swastikraychoudhuri@gmail.com>
2024-11-19 18:41:44 +0530

    bug fixes

--- src/component/AppHelmet.tsx
+++ src/component/AppHelmet.tsx
@@ -44,7 +44,7 @@ function AppHelmet() {
 
       {/* Preload Fonts */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;400;700&display=swap"
         rel="stylesheet"
